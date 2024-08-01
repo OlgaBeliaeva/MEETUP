e9b4f336532db1662407825d26e2d3bd172fcf18
OlgaBeliaeva <olgabel505@web.de>
2024-08-01 13:18:00 +0200

    begin script

---
+++ script.js
@@ -0,0 +1,4 @@
+import { eventsStore } from ".data.js";
+import  { createDomElement } from "./utils";
+import { formatDate } from "./utils";
+const allEventsDiv = document.querySelector(".all-events");
